d5dbe7853c5ee024909faf04492a8456958bd297
handagenalini <handagenalini@gmail.com>
2023-07-31 13:36:19 +0530

    odd length subarrays

--- javascript.js
+++ javascript.js
@@ -1,10 +1,20 @@
-let account= [[1,2,3],[3,2,1]]
-let max=0
-for(let i=0;i<account.length;i++){
-let sum=account[i].reduce((acc,num)=>{
-    return acc+num
-},0)
-// console.log(sum)
-max=Math.max(max,sum)
+let arr = [1,4,2,5,3]
+let l=0
+let r=arr.length-1
+let sum=0
+for(let i=0;i<arr.length;i++){
+    r=arr.length-1
+    while(l<r){
+        if((r-l)%2!=0){
+            for(let j=0;j<=(r-l);j++){
+                sum+=arr[j]
+            }
+        
+
+        }
+        r--
+    }
+    l++
+
 }
-console.log(max)
+console.log(sum)
