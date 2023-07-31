5b9f8bcae54773164f2f2bc84b4d18fd17ab7491
handagenalini <handagenalini@gmail.com>
2023-07-31 13:42:35 +0530

    bubble sort

--- javascript.js
+++ javascript.js
@@ -1,20 +1,10 @@
-let arr = [1,4,2,5,3]
-let l=0
-let r=arr.length-1
-let sum=0
+// bubble sort
+let arr=[2,4,1,5,3,6]
 for(let i=0;i<arr.length;i++){
-    r=arr.length-1
-    while(l<r){
-        if((r-l)%2!=0){
-            for(let j=0;j<=(r-l);j++){
-                sum+=arr[j]
-            }
-        
-
+    for(let j=0;j<arr.length-i-1;j++){
+        if(arr[j]>arr[j+1]){
+            [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
         }
-        r--
     }
-    l++
-
 }
-console.log(sum)
+console.log(arr)
