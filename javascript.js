6df1383c831f8c8f6d099a50496c3caae27dc29f
handagenalini <handagenalini@gmail.com>
2023-07-31 13:07:33 +0530

    store n prime numbers in an array

--- javascript.js
+++ javascript.js
@@ -1,2 +1,24 @@
+// store n prime numbers in an array
+let n=5
+let i=2
+let arr=[]
+while(arr.length<n){
+    if(isprime(i)){
+        arr.push(i)
+      
+ }
+ i++
+}
+function isprime(num){
+    if(num<2){
+        return false
+    }
+    for(let i=2;i<=Math.sqrt(num);i++){
+        if(num%i==0){
+            return false
+        }
+    }
+    return true
 
-
+}
+console.log(arr)
