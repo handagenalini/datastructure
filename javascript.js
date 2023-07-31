94387caf3639b3c324433a5644e2d99b1ab6e919
handagenalini <handagenalini@gmail.com>
2023-07-31 15:04:59 +0530

    functions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-
+class student{
+    constructor(name,rollno){
+        this.name=name,
+        this.rollno=rollno
+    }
+    display(){
+        console.log('my name is '+this.name+' this is my roll no '+this.rollno)
+    }
+}
+let c=new student('jhon',12)
+c.display()
