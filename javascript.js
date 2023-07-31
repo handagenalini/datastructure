7f229ac334c296571bf4e2731882793d388521a5
handagenalini <handagenalini@gmail.com>
2023-07-31 13:50:09 +0530

    selection sort

--- javascript.js
+++ javascript.js
@@ -1,12 +1,15 @@
 // bubble sort
 let arr=[2,4,1,5,3,6]
 for(let i=0;i<arr.length;i++){
-    let current=arr[i]
-    let j=i-1
-    while(j>=0 && current<arr[j]){
-        arr[j+1]=arr[j]
-        j--
+    let min=i
+ for(let j=i+1;j<arr.length;j++){
+    if(arr[min]<arr[j]){
+        min=j
     }
-    arr[j+1]=current
+
+ }
+ if(min!=i){
+    [arr[min],arr[i]]=[arr[i],arr[min]]
+ }
 }
 console.log(arr)
