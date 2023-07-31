834fe90660f1ba6e4bc1bf5584156cb2940c97a8
handagenalini <handagenalini@gmail.com>
2023-07-31 15:21:32 +0530

    functions

--- javascript.js
+++ javascript.js
@@ -1,11 +1,20 @@
 class student{
+    static count=0
     constructor(name,rollno){
         this.name=name,
         this.rollno=rollno
+        student.count++
     }
     display(){
         console.log('my name is '+this.name+' this is my roll no '+this.rollno)
     }
+    static countobj(){
+        console.log(student.count)
+
+    }
 }
 let c=new student('jhon',12)
+let a=new student('jhon',12)
+
 c.display()
+student.countobj()
