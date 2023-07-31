7485fd154234bbf709ef8a80f2e21f2fd616e6d0
handagenalini <handagenalini@gmail.com>
2023-07-31 18:38:09 +0530

    queue

--- javascript.js
+++ javascript.js
@@ -1,45 +1,39 @@
-class minstack{
+class queue{
     constructor(){
-        this.stack=[],
-        this.minstack=[]
+        this.queue=[]
     }
-    push(val){
-        this.stack.push(val)
-        if(this.minstack.length>0){
-            let minval=Math.min(val,this.minstack[this.minstack.length-1])
-            this.minstack.push(minval)
-
-        }else{
-            this.minstack.push(val)
-        }
+    enqueue(val){
+        this.queue.push(val)
 
     }
-    pop(){
+    dequeue(){
         if(!this.isempty()){
-            this.minstack.pop()
-            return this.stack.pop()
+            return this.queue.shift()
         }
+
+    }
+    front(){
+        return this.queue[0]
+
+    }
+    rear(){
+        return this.queue[this.queue.length-1]
 
     }
     isempty(){
-        return this.stack.length===0 && this.minstack.length===0
+        return this.queue.length===0
 
     }
-    getmin(){
-        return this.minstack[this.minstack.length-1]
 
-    }
-    top(){
-        return this.stack[this.stack.length-1]
-    }
 }
-let c=new minstack()
-c.push(1)
-c.push(2)
-c.push(3)
-c.push(4)
-console.log(c.pop())
-console.log(c.top())
-console.log(c.getmin())
-console.log(c.pop())
+let c=new queue()
+c.enqueue(1)
+c.enqueue(2)
+c.enqueue(3)
+c.enqueue(4)
+c.enqueue(5)
+console.log(c.dequeue())
+console.log(c.front())
+console.log(c.rear())
+console.log(c.dequeue())
 
