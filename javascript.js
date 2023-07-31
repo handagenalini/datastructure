07d9cfbc2a0c3b28bba230add9d627f780ac9b91
handagenalini <handagenalini@gmail.com>
2023-07-31 13:25:44 +0530

    running sum of array

--- javascript.js
+++ javascript.js
@@ -1,10 +1,8 @@
-//maximum sum subarray 
-let arr=[5,2,-4,-5, 3,-1,2,3,1]
-let currsum=arr[0]
-let max=arr[0]
+let arr=[1,2,3,4]
+let res=[arr[0]]
+let sum=arr[0]
 for(let i=1;i<arr.length;i++){
-    currsum=Math.max(arr[i],currsum+arr[i])
-    max=Math.max(currsum,max)
-
+    sum+=arr[i]
+    res.push(sum)
 }
-console.log(max)
+console.log(res)
