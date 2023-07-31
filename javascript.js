b1a0de8bbc1397a270e3eaff4be90d4f9625fa4b
handagenalini <handagenalini@gmail.com>
2023-07-31 13:30:15 +0530

    maximum wealth

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
-let arr=[1,2,3,4]
-let res=[arr[0]]
-let sum=arr[0]
-for(let i=1;i<arr.length;i++){
-    sum+=arr[i]
-    res.push(sum)
+let account= [[1,2,3],[3,2,1]]
+let max=0
+for(let i=0;i<account.length;i++){
+let sum=account[i].reduce((acc,num)=>{
+    return acc+num
+},0)
+// console.log(sum)
+max=Math.max(max,sum)
 }
-console.log(res)
+console.log(max)
