8f010e6a16bc692ac680b13311c381750e2fdbf4
handagenalini <handagenalini@gmail.com>
2023-07-31 16:03:48 +0530

    minstack

--- javascript.js
+++ javascript.js
@@ -1,20 +1,45 @@
-class student{
-    static count=0
-    constructor(name,rollno){
-        this.name=name,
-        this.rollno=rollno
-        student.count++
+class minstack{
+    constructor(){
+        this.stack=[],
+        this.minstack=[]
     }
-    display(){
-        console.log('my name is '+this.name+' this is my roll no '+this.rollno)
-    }
-    static countobj(){
-        console.log(student.count)
+    push(val){
+        this.stack.push(val)
+        if(this.minstack.length>0){
+            let minval=Math.min(val,this.minstack[this.minstack.length-1])
+            this.minstack.push(minval)
 
+        }else{
+            this.minstack.push(val)
+        }
+
+    }
+    pop(){
+        if(!this.isempty()){
+            this.minstack.pop()
+            return this.stack.pop()
+        }
+
+    }
+    isempty(){
+        return this.stack.length===0 && this.minstack.length===0
+
+    }
+    getmin(){
+        return this.minstack[this.minstack.length-1]
+
+    }
+    top(){
+        return this.stack[this.stack.length-1]
     }
 }
-let c=new student('jhon',12)
-let a=new student('jhon',12)
+let c=new minstack()
+c.push(1)
+c.push(2)
+c.push(3)
+c.push(4)
+console.log(c.pop())
+console.log(c.top())
+console.log(c.getmin())
+console.log(c.pop())
 
-c.display()
-student.countobj()
