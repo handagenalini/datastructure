11c021a13846de46560f132f32143fded545c4f3
handagenalini <handagenalini@gmail.com>
2023-07-31 18:47:55 +0530

    circularqueue

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
 class queue{
     constructor(){
         this.queue=[]
+        this.k=5
     }
     enqueue(val){
+        if(!this.isfull()){
         this.queue.push(val)
+        }
 
     }
     dequeue(){
@@ -24,6 +27,9 @@ class queue{
         return this.queue.length===0
 
     }
+    isfull(){
+        return this.queue.length==this.k
+    }
 
 }
 let c=new queue()
@@ -36,4 +42,14 @@ console.log(c.dequeue())
 console.log(c.front())
 console.log(c.rear())
 console.log(c.dequeue())
+c.enqueue(1)
+console.log(c.dequeue())
+console.log(c.dequeue())
+console.log(c.front())
+
+
+c.enqueue(2)
+c.enqueue(3)
+c.enqueue(4)
+c.enqueue(5)
 
