bd68c60e1ba82a383622ac42cceaf9d54b83beb4
handagenalini <handagenalini@gmail.com>
2023-08-01 09:24:26 +0530

    splitstring

--- javascript.js
+++ javascript.js
@@ -1,44 +1,4 @@
-class listnode{
-    constructor(val,next){
-        this.val=val,
-        this.next=next
-    }
-}
-function cycle(head){
-    if(head==null){
-        return
-    }
-    let fast=head
-    let slow=head 
-    while(fast && fast.next){
-        fast=fast.next.next
-        slow=slow.next
-        if(slow==fast){
-            slow=head
-            while(slow!=fast){
-                slow=slow.next
-                fast=fast.next
-            }
-            return slow
-        }
-    }
-    return null
-
-
-}
-let k=2
-// console.log(rotetatelist(head,k))
-let node5=new listnode(6)
-let node4=new listnode(5,node5)
-let node3=new listnode(4,node4)
-let node2=new listnode(3,node3)
-
-
-let node1=new listnode(2,node2)
-let head=new listnode(1,node1)
-console.log(cycle(head))
-console.log(head)
-
-  
-
+let str='my name is jerry,i want to be,in thr.disney worid,you are|with me  so are u ,ready: for the journey'
+let string=str.split(/[\s,|.:]+/)
+console.log(string)
 
