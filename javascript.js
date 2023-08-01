f52218255d7eca61ad2dfd797f9fd8344165cd88
handagenalini <handagenalini@gmail.com>
2023-08-01 10:30:13 +0530

    membershipactive

--- javascript.js
+++ javascript.js
@@ -1,29 +1,31 @@
-function Person(firstName, lastName, age, sex) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.sex = sex;
-  
-    this.printFullName = function () {
-      console.log(this.firstName + " " + this.lastName);
-    };
-  }
-  
-  const person1 = new Person("yash", "prasad", 10, "M");
-  const person2 = new Person("vaibhav", "prasad", 20, "M");
-  
-  function isEligibleForDisco(person) {
-    if(person.age >= 18){
-        return person.firstName +' is eligible'
-    };
-    return person.firstName +' is not eligible'
-  }
-  
-  // Test if persons are eligible for the disco
-  console.log(person1.firstName + " is eligible for disco: " + isEligibleForDisco(person1));
-  console.log(person2.firstName + " is eligible for disco: " + isEligibleForDisco(person2));
-  
-  // Output:
-  // yash is eligible for disco: false
-  // vaibhav is eligible for disco: true
-  
+
+// pyash,yash@gmail.com,password123,MONTHLYPACKAGE
+class person{
+    constructor(name,email,password,packagetype){
+        this.name=name,
+        this.email=email,
+        this.password=password,
+        this.packagetype=packagetype
+        this.date=new Date('2023-08-01')
+    }
+    print(){
+        if(this.packagetype==='monthlypackage'){
+            const newdate=new Date(this.date)
+        
+        newdate.setMonth(this.date.getMonth()+1)
+        return 'membership active date '+newdate.toDateString()
+        }else{
+        const newdate=new Date(this.date)
+        newdate.setFullYear(this.date.getFullYear()+1)
+        return 'membership active date '+newdate.toDateString()
+
+
+        }
+    }
+
+}
+let person1=new person('pyash','yash@gmail.com','yash','monthlypackage')
+console.log(person1.print())
+let person2=new person('pvaibhav','vaibhav@gmail.com','vaibhav','yearlypackage')
+
+console.log(person2.print())
