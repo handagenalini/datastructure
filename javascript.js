f474a7c9b559c73007cbd94411a0d5711866e59f
handagenalini <handagenalini@gmail.com>
2023-08-01 10:10:04 +0530

    eligible

--- javascript.js
+++ javascript.js
@@ -1,4 +1,29 @@
-let str='my name is jerry,i want to be,in thr.disney worid,you are|with me  so are u ,ready: for the journey'
-let string=str.split(/[\s,|.:]+/)
-console.log(string)
-
+function Person(firstName, lastName, age, sex) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.sex = sex;
+  
+    this.printFullName = function () {
+      console.log(this.firstName + " " + this.lastName);
+    };
+  }
+  
+  const person1 = new Person("yash", "prasad", 10, "M");
+  const person2 = new Person("vaibhav", "prasad", 20, "M");
+  
+  function isEligibleForDisco(person) {
+    if(person.age >= 18){
+        return person.firstName +' is eligible'
+    };
+    return person.firstName +' is not eligible'
+  }
+  
+  // Test if persons are eligible for the disco
+  console.log(person1.firstName + " is eligible for disco: " + isEligibleForDisco(person1));
+  console.log(person2.firstName + " is eligible for disco: " + isEligibleForDisco(person2));
+  
+  // Output:
+  // yash is eligible for disco: false
+  // vaibhav is eligible for disco: true
+  
